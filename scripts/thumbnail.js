775c75d792f98e341dc198cd6f704535b989cbd8
J-Guenther <jannik.guentheg@gmail.com>
2022-08-15 21:44:29 +0200

    rerendering when updating page ordering or image

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -1,14 +1,15 @@
 class Thumbnail {
     static addToEntries(app, html) {
+        console.log('update journal')
         const lis = html.find('li.journalentry');
         for (const li of lis) {
             const target = $(li);
             const id = target.data('document-id');
             const journalEntry = game.journal.get(id);
             
-            if(journalEntry?.data?.document?.pages.size > 0) {
-                const iterator = journalEntry?.data?.document?.pages.values()
-                const firstJournalPage = iterator.next().value;
+            if(journalEntry?.pages.size > 0) {
+                const sortedArray = journalEntry.pages.contents.sort((a,b)=> a.sort - b.sort)
+                const firstJournalPage = sortedArray[0];
     
                 if (firstJournalPage.src) {
                     const thumbnail = $('<img class="thumbnail" src="' + firstJournalPage.src + '" alt="Journal Entry Thumbnail">');
@@ -17,6 +18,10 @@ class Thumbnail {
             }
         }
     }
+    static updateJournalDirectory(app, html) {
+        game.journal.render();
+    }
 }
 
 Hooks.on('renderJournalDirectory', Thumbnail.addToEntries);
+Hooks.on('renderJournalSheet', Thumbnail.updateJournalDirectory);
